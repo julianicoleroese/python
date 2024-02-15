9929a57c72aab4ebe3eb8082b66c4f067c1d978a
julia nicole <128698991+julianicoleroese@users.noreply.github.com>
2024-02-15 10:45:13 -0300

    Add files via upload

---
+++ soma.py
@@ -0,0 +1,5 @@
+#solicitar 2 numeros pro usuario, converter pra flowt, fazer a somar e exibir pro usuario
+
+numero = float (input ("Escreva um numero"))
+numero2 = float (input ("Escreva um numero"))
+
