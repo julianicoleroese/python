4d9c08c6dfe9cab76e2423ea307190aa072e0e84
julia nicole <128698991+julianicoleroese@users.noreply.github.com>
2024-02-20 09:26:17 -0300

    Add files via upload

--- soma.py
+++ soma.py
@@ -1,5 +1,9 @@
 #solicitar 2 numeros pro usuario, converter pra flowt, fazer a somar e exibir pro usuario
 
-numero = float (input ("Escreva um numero"))
-numero2 = float (input ("Escreva um numero"))
-
+try:
+    a = float (input ("Escreva um numero"))
+    b = float (input ("Escreva um numero"))
+except ValueError:
+    print("Tente Novamente")
+soma = a + b 
+print(f"Soma de {a} e {b} é igual a:{soma}")
