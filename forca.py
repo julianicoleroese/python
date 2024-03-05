ffa79ce05c1903a06105514c02a2403cdc56b643
julia nicole <128698991+julianicoleroese@users.noreply.github.com>
2024-03-05 08:13:37 -0300

    Add files via upload

--- forca.py
+++ forca.py
@@ -1,23 +1,27 @@
-palavra_secreta = "pindamonhangaba"
-letras_acertadas = ["_","_","_","_","_","_","_","_","_","_","_","_","_","_","_"]
-tentativas = 10 
+def jogar():    
+    palavra_secreta = "pindamonhangaba"
+    letras_acertadas = ["_","_","_","_","_","_","_","_","_","_","_","_","_","_","_"]
+    tentativas = 10 
 
-while tentativas > 0 and "_" in letras_acertadas:
-    palpite = input("Digite uma letra: ").lower()
+    while tentativas > 0 and "_" in letras_acertadas:
+        palpite = input("Digite uma letra: ").lower()
     
-    if palpite in palavra_secreta:
-        index = 0
-        for letra in palavra_secreta:
-            if palpite == letra:
-                letras_acertadas[index] = letra
-            index += 1 
+        if palpite in palavra_secreta:
+            index = 0
+            for letra in palavra_secreta:
+                if palpite == letra:
+                    letras_acertadas[index] = letra
+                index += 1 
+        else:
+            tentativas -= 1 
+            print(f"Você tem {tentativas} restantes")
+        
+        print(" ".join(letras_acertadas))
+        
+    if "_" not in letras_acertadas:
+        print("Parabéns você ganhou!")
     else:
-        tentativas -= 1 
-        print(f"Você tem {tentativas} restantes")
-        
-    print(" ".join(letras_acertadas))
-        
-if "_" not in letras_acertadas:
-    print("Parabéns você ganhou!")
-else:
-    print(f"Você perdeu piazinho, a palavra era {palavra_secreta}")
+        print(f"Você perdeu piazinho, a palavra era {palavra_secreta}")
+
+id(__name__ == "__main__"):
+    jogar()
